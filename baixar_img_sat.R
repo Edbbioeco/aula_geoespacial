ff62bf9c9addee347e0926387c6c229313fc5667
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-26 10:47:52 -0300

    Visualizar o raster da imagem de satélite de Recife

--- baixar_img_sat.R
+++ baixar_img_sat.R
@@ -30,3 +30,12 @@ ggplot() +
 rec_sat <- rec |>
   maptiles::get_tiles(provider = "Esri.WorldImagery",
                       zoom = 14)
+
+## Visualizar ----
+
+rec_sat
+
+ggplot() +
+  tidyterra::geom_spatraster_rgb(data = rec_sat) +
+  geom_sf(data = rec, color = "red", fill = "transparent", linewidth = 1) +
+  coord_sf(expand = FALSE)
