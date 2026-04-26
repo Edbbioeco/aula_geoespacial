96ac234ff19244a20fb824a8b1d07243b00de4b2
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-26 10:59:47 -0300

    Importar visualizar o shapefile do Brasil

--- confeccionar_mapas.R
+++ confeccionar_mapas.R
@@ -15,3 +15,18 @@ library(ggview)
 library(ggmagnify)
 
 library(patchwork)
+
+# Dados ----
+
+## Shapefile do Brasil ----
+
+### Importar ----
+
+br <- sf::st_read("br.shp")
+
+### Visualizar ----
+
+br
+
+ggplot() +
+  geom_sf(data = br, color = "black")
